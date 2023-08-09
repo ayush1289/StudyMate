e241074f8bc6775823d93e4bd21704212665f3f2
Ayush Shankarpurkar <ayushshankarpurkar11@gmail.com>
2023-08-09 20:17:36 +0530

    initial commit

---
+++ dashboard/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+
+# Create your models here.
+class Notes(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name="notes"
+        verbose_name_plural="notes"
+        
+class Homework(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    subject= models.CharField(max_length=50)
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    due=models.DateTimeField()
+    is_finished=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+class DashboardForm(forms.Form):
+    text= forms.CharField(max_length=100,label="Enter your search : ")
+    
+
+class Todo(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title= models.CharField(max_length=100)
+    is_finished=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name="Todo"
+        verbose_name_plural="Todo"
+        
